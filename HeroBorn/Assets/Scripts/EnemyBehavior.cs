f2fd7054a7fb0613c5ef355c2b6f1308122494b1
EverettKelsey <kelseyj@my.erau.edu>
2024-01-31 16:35:47 -0700

    added movement

---
+++ HeroBorn/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    void onTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            Debug.Log("Player detected - attack!");
+        }
+    }
+    void onTriggerExit(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            Debug.Log("Player out of range, resume patrol");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
