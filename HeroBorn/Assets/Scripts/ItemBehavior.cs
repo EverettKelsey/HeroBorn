d2b30f6fce84af677d85ed91568824e030c77f98
EverettKelsey <kelseyj@my.erau.edu>
2024-02-07 12:22:28 -0700

    added jump and shoot

--- HeroBorn/Assets/Scripts/ItemBehavior.cs
+++ HeroBorn/Assets/Scripts/ItemBehavior.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class ItemBehavior : MonoBehaviour
 {
+    public GameBehavior gameManager;
+
+    void Start()
+    {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameBehavior>();
+    }
     void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.name == "Player")
         {
             Destroy(this.transform.parent.gameObject);
             Debug.Log("Item collected!");
+            gameManager.Items += 1;
         }
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
     }
 
     // Update is called once per frame
